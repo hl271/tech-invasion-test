7c98c12c9bcbdbe2e21526a0aaef6cda2c7ad6a5
Lan Dang <danghoanglan271@gmail.com>
2018-08-09 04:02:02 +0700

    Env changed

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.projectId,
       clientEmail: process.env.clientEmail,
-      privateKey: process.env.privateKey
+      privateKey: process.env.privateKey.replace(/\\n/g, '\n')
     }),
     databaseURL: process.env.databaseURL
   })
